751b1213154a010163366f65122134458a6b5844
Mrbrro <sasarogacov@gmail.com>
2025-08-01 19:22:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,17 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class Article(db.Model):
-     __tablename__ = 'articles'
-     id = db.Column(db.Integer, primary_key=True)
-     title = db.Column(db.String(100), nullable=False)
-     intro = db.Column(db.String(300), nullable=False)
-     text = db.Column(db.Text, nullable=False)
-     date = db.Column(db.DateTime, default=datetime.utcnow)
+    __tablename__ = 'articles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
     def __repr__(self):
-        return "<Article %r>" % self.id
+        return f"<Article {self.id}>"
+
 
 with app.app_context():
         db.create_all()
