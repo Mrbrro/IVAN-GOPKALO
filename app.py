3bbec1448afd564ef3e51c87143d2589d6f1d6cc
Mrbrro <sasarogacov@gmail.com>
2025-08-01 19:19:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,11 @@ db = SQLAlchemy(app)
 
 class Article(db.Model):
      __tablename__ = 'articles'
-    id = db.Column(db.Integer, primary_key= True)
-    title = db.Column(db.String(100), nullable=False)
-    intro = db.Column(db.String(300), nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
-
+     id = db.Column(db.Integer, primary_key=True)
+     title = db.Column(db.String(100), nullable=False)
+     intro = db.Column(db.String(300), nullable=False)
+     text = db.Column(db.Text, nullable=False)
+     date = db.Column(db.DateTime, default=datetime.utcnow)
     def __repr__(self):
         return "<Article %r>" % self.id
 
